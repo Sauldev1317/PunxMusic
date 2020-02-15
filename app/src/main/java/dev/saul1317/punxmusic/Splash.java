c6b5519075fe73673f64333fbf8de824d5bc68b9
Saúl Alejandro Delgado Mayorga <saulalejandrodelgadomayorga@MacBook-Air-de-Saul.local>
2020-02-14 20:49:30 -0600

    Initial commit

---
+++ app/src/main/java/dev/saul1317/punxmusic/Splash.java
@@ -0,0 +1,14 @@
+package dev.saul1317.punxmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
