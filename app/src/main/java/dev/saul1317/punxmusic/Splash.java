0187f628aed97108e86d08719fdb69de0d599c9b
saulalejandrodelgadomayorga <dev.delgado1317@gmail.com>
2020-02-17 18:02:21 -0600

    Funcionalidad del splash completada

--- app/src/main/java/dev/saul1317/punxmusic/Splash.java
+++ app/src/main/java/dev/saul1317/punxmusic/Splash.java
@@ -2,13 +2,70 @@ package dev.saul1317.punxmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Splash extends AppCompatActivity {
+
+    public String TAG = "Splash";
+    public final int DURATION_SPLASH = 2000;
+
+    //LottieAnimationView animation_view;
+
+    private Handler handler;
+    private Runnable myRunnable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        loadUI();
     }
+
+    private void loadUI() {
+        //animation_view = (LottieAnimationView) findViewById(R.id.animation_view);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        durationSplash();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(myRunnable);
+    }
+
+    public void durationSplash(){
+        //hilo para abrir la siguiente ventana despues despues de tres segundos
+        handler = new Handler();
+        myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                openNewActivity();
+            }
+        };
+        handler.postDelayed(myRunnable, DURATION_SPLASH);
+    }
+
+
+    //Abrir la siguiente ventana
+    private void openNewActivity() {
+        Intent intent = new Intent(this, Home.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        //overridePendingTransition( 0, R.anim.fade_out_animation);
+    }
+
 }
