0a22bbc3e1bd620dbf0de505457df959eea8a4df
saulalejandrodelgadomayorga <dev.delgado1317@gmail.com>
2020-02-17 19:30:04 -0600

    Diseño de Toolbar en home

--- app/src/main/java/dev/saul1317/punxmusic/Home.java
+++ app/src/main/java/dev/saul1317/punxmusic/Home.java
@@ -1,14 +1,50 @@
 package dev.saul1317.punxmusic;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    Toolbar home_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        loadUI();
+    }
+
+    private void loadUI() {
+        home_toolbar = (Toolbar) findViewById(R.id.toolbar);
+        home_toolbar.setLogo(R.drawable.punxmusic_logo_appbar);
+        home_toolbar.setNavigationIcon(R.drawable.burger_icon);
+        setSupportActionBar(home_toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
